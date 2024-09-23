15755e8d74c03ca430524a82bd219e3acbe2bed7
KanchonBauri <kanchonbruei1998@gmail.com>
2024-09-23 16:42:29 +0600

    history section

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ const feniBalanceEl2 = document.getElementById('feni-balance')
 const donationInputEl3 = document.getElementById('donation-input3')
 const quotaBalanceEl3 = document.getElementById('quota-balance')
 
+const historyContainer = document.getElementById('history-container')
 
 // -----------------------------------noakhali ------------------------------
 function donationHandle(event) {
@@ -17,7 +18,10 @@ function donationHandle(event) {
     const donationInput = (donationInputEl.value);
     if (isNaN(donationInput) || donationInput <= 0) {
         return alert('invalid input')
+    } else {
+        alert('wow')
     }
+
     const donationInputValue = parseFloat(donationInput);
     const currentBalance = parseFloat(currentBalanceEl.innerText);
     // console.log(donationInputValue, currentBalance)
@@ -31,16 +35,27 @@ function donationHandle(event) {
     // console.log(newNoakhaliBalance)
     noakhaiBalanceEl.innerText = newNoakhaliBalance;
 
-// -----------------------------------------------------------------------------------------------------------------------------------------
+    // history list 
+    const historyList = document.createElement("div");
+    historyList.className = " rounded-xl border border-red-500 font-semibold p-5 mb-5";
+
+    historyList.innerHTML = `
+    <p class="text-black text-xl">${donationInputValue} Taka is Donated for Food at Noakhali, Bangladesh</p>
+    <p class="text-gray-500" >${new Date().toLocaleDateString()} +6066 (Bangladesh Standard Time)</p>
+
+    `
+    historyContainer.insertBefore(historyList, historyContainer.firstChild)
+
+    // -----------------------------------------------------------------------------------------------------------------------------------------
 
 
 
 
 
-// return modal1;
+    // return modal1;
 
 
-// -----------------------------------------------------------------------------------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------------------------------------
 }
 
 // -----------------------------------------------feni---------------------------------------
@@ -62,6 +77,17 @@ function donationHandle2(event) {
     const newfeniBalance = feniBalance + donationInputValue;
     // console.log(newNoakhaliBalance)
     feniBalanceEl2.innerText = newfeniBalance;
+
+    // history list 
+    const historyList = document.createElement("div");
+    historyList.className = " rounded-xl border border-red-500 font-semibold p-5 mb-5 bg-white ";
+
+    historyList.innerHTML = `
+     <p class="text-black text-xl">${donationInputValue} Taka is Donated for Flood Relief in Feni, Bangladesh</p>
+     <p class="text-gray-500" >${new Date().toLocaleDateString()} +6066 (Bangladesh Standard Time)</p>
+ 
+     `
+    historyContainer.insertBefore(historyList, historyContainer.firstChild)
 }
 function donationHandle3(event) {
     // console.log('hello')
@@ -80,5 +106,47 @@ function donationHandle3(event) {
     const newNquotaBalance = quotaBalance + donationInputValue;
     // console.log(newNoakhaliBalance)
     quotaBalanceEl3.innerText = newNquotaBalance;
+
+    // history list 
+    const historyList = document.createElement("div");
+    historyList.className = " rounded-xl border border-red-500 font-semibold p-5 mb-5  bg-white ";
+
+    historyList.innerHTML = `
+     <p class="text-black text-xl">${donationInputValue} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
+     <p class="text-gray-500" >${new Date().toLocaleDateString()} +6066 (Bangladesh Standard Time)</p>
+ 
+     `
+    historyContainer.insertBefore(historyList, historyContainer.firstChild)
 }
+
+// ===========================================================================================================
+// history section 
+const donationTab = document.getElementById('donation-tab');
+const historyTab = document.getElementById('history-tab');
+const mainSection = document.getElementById('main-section')
+
+historyTab.addEventListener('click', function () {
+    historyTab.classList.add("bg-[#B4F461]", "focus:border-none", "duration-200", "text-white");
+
+    donationTab.classList.remove("bg-[#B4F461]", "focus:border-none", "duration-200", "text-white");
+
+    mainSection.classList.add('hidden');
+
+    document.getElementById("history-section").classList.remove("hidden")
+
+
+
+})
+donationTab.addEventListener('click', function () {
+    historyTab.classList.remove("bg-[#B4F461]", "focus:border-none", "duration-200", "text-white");
+
+    donationTab.classList.add("bg-[#B4F461]", "focus:border-none", "duration-200", "text-white");
+
+    mainSection.classList.remove('hidden');
+
+    document.getElementById("history-section").classList.add("hidden")
+
+
+
+})
 
